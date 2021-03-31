085b308225fb9d375f8b84e9be879c48431dd23e
arrianne <arrianneoshea@hotmail.co.uk>
2021-03-31 11:23:50 +1000

    updating main.js to handle authentication state changes

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { auth } from './firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+// Import your Firebase configuration and set up the Firebase method onAuthStateChanged. This ensures Firebase initializes before loading the app when a user refreshes a page.
+let app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
